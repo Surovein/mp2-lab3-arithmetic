a32687fc1f55d9b0994a7176fab8eba2c3997365
Evgeny_Surovegin <evgeny.surovegin@gmail.com>
2025-10-13 23:24:56 +0300

    Lab3

--- include/stack.h
+++ include/stack.h
@@ -7,3 +7,65 @@
 // - получение количества элементов в стеке
 // - очистка стека
 // при вставке в полный стек должна перевыделяться память
+template<typename T>
+class Stack
+{
+protected:
+	T* pMem;
+	int top_index;
+	int size;
+	Stack(sz)
+	{
+		size = sz;
+		T* pMem = new T[size];
+		top_index = -1;
+	}
+public:
+	void push( T value)
+	{
+		if (top_index + 1 < size)
+		{
+			top_index += 1;
+			pMem[top_index] = value;
+		}
+		else
+		{
+			T* tmp = new T[size*2];
+			for (int i = 0; i < top_index; i++)
+			{
+				tmp[i] = pMem[i];
+			}
+			delete pMem;
+			pMem = tmp;
+			top_index += 1;
+			pMem[top_index] = value;
+			size *= 2;
+		}
+	}
+	T top()
+	{
+		return pMem[top_index];
+	}
+	T pop()
+	{
+		T tmp = pMem[top_index];
+		top_index -= 1;
+		return tmp;
+	}
+	bool emptiness_сheck()
+	{
+		return (top_index == -1);
+	}
+	int stack_real_size()
+	{
+		return(top_index + 1);
+	}
+	void delete_stack()
+	{
+		top_index = -1;
+	}
+	~stack()
+	{
+		delete[] pMem;
+	}
+};
