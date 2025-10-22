2af5479347139ea0a14de409029d5bfd9be09ed4
Евгений С <evgeny.surovegin@gmail.com>
2025-10-22 14:08:32 +0300

    Lab3

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -5,11 +5,20 @@
 #include<map>
 #include"stack.h"
 using namespace std;
+
+
+enum LEXEM_TYPE {
+	OPERAND,
+	OPERATION,
+	BRACKET,
+	VARIABLE
+};
+
 struct Lexema
 {
 	string stroka;
-	int type;
 	int index = -1;
+	LEXEM_TYPE typeLex;
 };
 class  TArithmeticExpression
 {
@@ -17,31 +26,177 @@ class  TArithmeticExpression
 	string postfix;
 	vector<Lexema> postfix;
 	vector<Lexema> lexems;;// str[0] == char сравнение строки с символом
-	vector<Lexema> lexems_correct;
 	map<string, int> priority =
 	{
 		{ "+",1}, {"-", 1}, {"*",2}, {"/", 2},{"(", 0}
 	};
-	map<string, int> type =
-	{
-		{ "+",1}, {"-", 2}, {"*",3}, {"/", 4}
-	};
 	map<char, double> operands;// для переменных// шаблон
+
+
+
+
+
+public:
+
 	TArithmeticExpression(string stroka)
 	{
 		infix = stroka;
 	}
-	bool Parse()
-	{// 1) поиск скобок, удаление их 2)  разбиение на подстроки, получение лексем 3) проверка
-		int last_index = sizeof(infix) - 1;
+
+	vector<Lexema> getLexems()	
+	{
+		return lexems;
+	}
+
+	//bool Parse()
+	//{// 1) поиск скобок, удаление их 2)  разбиение на подстроки, получение лексем 3) проверка
+	//	int last_index = sizeof(infix) - 1;
+	//	int open = 0;
+	//	int close = 0;
+	//	int count_delete = 0;
+	//	int pos = 0;
+	//	while (infix.find_first_of("()") != -1) 
+	//	{
+	//		Lexema tmp;
+	//		tmp.stroka = infix[infix.find_first_of("()")];
+	//		if (tmp.stroka == "(")
+	//		{
+	//			open++;
+	//		}
+	//		else
+	//		{
+	//			close++;
+	//		}
+	//		tmp.type = 5;
+	//		tmp.index = infix.find_first_of("()") + count_delete +1; //+1 для того чтобы у 0 индекса индекс был не -1, те у всех операций будет+1 к индексу
+	//		lexems.push_back(tmp);
+	//		infix.erase(infix.find_first_of("()"), 1);
+	//		count_delete += 1;
+	//	}
+	//	if (open != close)
+	//	{
+	//		return false; // ошибка
+	//	}
+	//	while (infix.find_first_of("+*/-") != -1)// знак
+	//	{
+	//		int index = infix.find_first_of("+*/-");
+	//		Lexema tmp;
+	//		tmp.stroka = infix[infix.find_first_of("+*/-")];
+	//		auto it = type.find(tmp.stroka);
+	//		if (it != type.end())// объяснение
+	//		{
+	//			tmp.type = it->second;;
+	//		}
+	//		tmp.index = index + count_delete +1;
+	//		string substr_left = infix.substr(0, index);// элемент слева от знака
+	//		Lexema tmp2;
+	//		tmp2.stroka = substr_left;
+	//		auto it = type.find(tmp2.stroka);
+	//		if (it != type.end())
+	//		{
+	//			tmp2.type = it->second; // если зайдет сюда, значит два знака подряд => ошибка
+	//			return false;
+	//		}
+	//		else
+	//		{
+	//			tmp2.type = 0; // 0 - double
+	//		}
+	//		tmp2.index = tmp.index - 2;
+	//		lexems.push_back(tmp);
+	//		lexems.push_back(tmp2);
+	//		infix.erase(0,infix.find_first_of("+*/-")+1); // наверное так
+	//		count_delete += (infix.find_first_of("+*/-") + 1);
+	//	}
+	//	Lexema tmp;
+	//	tmp.stroka = infix;
+	//	auto it = type.find(tmp.stroka);
+	//	if (it != type.end())// объяснение
+	//	{
+	//		tmp.type = it->second;// если зайдет сюда, значит два знака подряд => ошибка
+	//		return false;
+	//	}
+	//	else
+	//	{
+	//		tmp.type = 0;
+	//	}
+	//	tmp.index = last_index;
+	//	lexems.push_back(tmp);  /// с помощью index сортирую получившийся вектор lexem и получаю правильный порядок лексем
+	//	return true;
+	//}
+
+
+	void Parse2()
+	{
 		int open = 0;
 		int close = 0;
-		int count_delete = 0;
-		while (infix.find_first_of("()") != -1) 
+		int pos = 0;
+
+
+		while (pos < infix.size()) {
+			string x = string(infix[pos], 1);
+			if (x.find_first_of("()") != -1)
+			{
+				Lexema tmp;
+				tmp.stroka = x;
+				if (tmp.stroka == "(")
+				{
+					open++;
+				}
+				else
+				{
+					close++;
+				}
+				tmp.typeLex = BRACKET;
+				lexems.push_back(tmp);
+				pos++;
+			}
+
+			else if (x.find_first_of("+*/-") != -1)// знак
+			{
+				Lexema tmp;
+				tmp.stroka = x;
+				tmp.typeLex = OPERATION;
+				lexems.push_back(tmp);
+				pos++;
+			}
+			else {
+
+				// aa + bb
+				if (isalpha(infix[pos])) {
+					// копим букву
+					Lexema tmp;
+					tmp.stroka = x;
+					tmp.typeLex = VARIABLE;
+					lexems.push_back(tmp);
+					pos++;
+				}
+				else if (isdigit(infix[pos])) {
+					pos++;
+					while (isdigit(infix[pos]) || (infix[pos] == '.'))
+					{
+						x += string(infix[pos], 1);
+						pos++;
+					}
+					// x = "123.45"
+					Lexema tmp;
+					tmp.stroka = x;
+					tmp.typeLex = OPERAND;
+					lexems.push_back(tmp);// где то еще нужно перевести в double
+				}
+			}
+		}
+	}
+	bool check()
+	{
+		int open = 0;
+		int close = 0;
+		for (int i = 0; i < sizeof(lexems) - 1; i++)
 		{
-			Lexema tmp;
-			tmp.stroka = infix[infix.find_first_of("()")];
-			if (tmp.stroka == "(")
+			if (lexems[i].typeLex == lexems[i + 1].typeLex && lexems[i].typeLex != BRACKET && lexems[i+1].typeLex!= BRACKET)
+			{
+				return false;
+			}
+			if (lexems[i].stroka == "(")
 			{
 				open++;
 			}
@@ -49,109 +204,21 @@ class  TArithmeticExpression
 			{
 				close++;
 			}
-			tmp.type = 5;
-			tmp.index = infix.find_first_of("()") + count_delete +1; //+1 для того чтобы у 0 индекса индекс был не -1, те у всех операций будет+1 к индексу
-			lexems.push_back(tmp);
-			infix.erase(infix.find_first_of("()"), 1);
-			count_delete += 1;
+			
 		}
-		if (open != close)
+		if (lexems[sizeof(lexems) - 1].stroka == "(")
 		{
-			return false; // ошибка
-		}
-		while (infix.find_first_of("+*/-") != -1)// знак
-		{
-			int index = infix.find_first_of("+*/-");
-			Lexema tmp;
-			tmp.stroka = infix[infix.find_first_of("+*/-")];
-			auto it = type.find(tmp.stroka);
-			if (it != type.end())// объяснение
-			{
-				tmp.type = it->second;;
-			}
-			tmp.index = index + count_delete +1;
-			string substr_left = infix.substr(0, index);// элемент слева от знака
-			Lexema tmp2;
-			tmp2.stroka = substr_left;
-			auto it = type.find(tmp2.stroka);
-			if (it != type.end())
-			{
-				tmp2.type = it->second; // если зайдет сюда, значит два знака подряд => ошибка
-				return false;
-			}
-			else
-			{
-				tmp2.type = 0; // 0 - double
-			}
-			//tmp2.index = tmp.index - 2;
-			lexems.push_back(tmp);
-			lexems.push_back(tmp2);
-			infix.erase(0,infix.find_first_of("+*/-")+1); // наверное так
-			count_delete += (infix.find_first_of("+*/-") + 1);
-		}
-		Lexema tmp;
-		tmp.stroka = infix;
-		auto it = type.find(tmp.stroka);
-		if (it != type.end())// объяснение
-		{
-			tmp.type = it->second;// если зайдет сюда, значит два знака подряд => ошибка
-			return false;
+			open++;
 		}
 		else
 		{
-			tmp.type = 0;
+			close++;
 		}
-		//tmp.index = last_index;
-		lexems.push_back(tmp);  /// с помощью index сортирую получившийся вектор lexem и получаю правильный порядок лексем
+		if (close != open)
+		{
+			return false;
+		}
 		return true;
-	}
-	void sort()
-	{
-		int n = lexems.size();
-
-		for (int i = 0; i < n - 1; i++)
-		{
-			for (int j = 0; j < n - i - 1; j++)
-			{
-				if (lexems[j].index > lexems[j + 1].index)
-				{											// Меняем элементы местами
-					swap(lexems[j], lexems[j + 1]);
-				}
-			}
-		}
-	}
-	void create_correct_vector()
-	{
-		int index = 0;
-		while (lexems[index].index == -1)
-		{
-			index++;
-		}
-		index++; // получаем индекс первого знака
-		//lexems_correct.push_back(lexems[index]);// по идее нужно сделать 2 случая 1) скобка в начале 2) знак в начале
-		int i = 0;
-		for (int index; index < lexems.size(); i++)
-		{
-			//if (index = lexems.size() - 1)
-			//{
-			//	lexems_correct.push_back(lexems[index]);
-			//	lexems_correct.push_back(lexems[i]);
-			//}
-			if (lexems[index].index != lexems[index + 1].index + 1)
-			{
-				lexems_correct.push_back(lexems[i]);
-				lexems_correct.push_back(lexems[index + 1]);
-				index++;
-				i++;
-			}
-			else
-			{
-				lexems_correct.push_back(lexems[index + 1]);
-				index++;
-			}
-			//lexems_correct.push_back(lexems[index]);
-			if()
-		}
 	}
 	void ToPostfix(); // построение постфиксной записи
 	string GetInfix() const 
--- test/test_arithmetic.cpp
+++ test/test_arithmetic.cpp
@@ -1,3 +1,9 @@
 // тесты для вычисления арифметических выражений
 
 #include <gtest.h>
+#include "arithmetic.h"
+
+TEST(arithm, test1) {
+	string x = "1+1";
+	vector<Lexema> v = { {"1", OPERAND}, {"+", OPERATION}, {"1", OPERAND} };
+}
